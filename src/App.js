a57b492c17d63d4c00e457bd840585c0185b0113
james-99k <jamesgasty@gmail.com>
2021-06-07 21:00:26 +0800

    task 4, 5

--- src/App.js
+++ src/App.js
@@ -1,59 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState, Component } from 'react'
 import User from './components/User'
 import Navbar from './components/Navbar'
 
-function App() {
-  let state = {
-    userA: {
-      firstName: "Shinya",
-      avatarUrl: "https://www.rakuten-sec.co.jp/web/shared/images/og-image.gif"
-    },
-    userB: {
-      firstName: "Hayato",
-      avatarUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Rakuten_Global_Brand_Logo.svg/440px-Rakuten_Global_Brand_Logo.svg.png"
-    },
-    backColor: 'yellow',
-    bootcamp: 'Rakuten Shoken'
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userA: {
+        firstName: "Shinya",
+        avatarUrl: "https://www.rakuten-sec.co.jp/web/shared/images/og-image.gif"
+      },
+      userB: {
+        firstName: "Hayato",
+        avatarUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Rakuten_Global_Brand_Logo.svg/440px-Rakuten_Global_Brand_Logo.svg.png"
+      },
+      backColor: 'yellow',
+      bootcamp: 'Rakuten Shoken'
+    };
+
+    // const [clickCount, setState] = useState(0);
+
+    let colorMapper = () => {
+      const hexColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      return hexColor;
+    };
+
+    let clickHandler = () => {
+      if (this.clickCount % 5 === 0) {
+        this.setState({
+          backColor: colorMapper(),
+          clickCount: (this.clickCount + 1)
+        });
+      }
+      else {
+        this.setState({ clickCount: this.clickCount + 1 });
+      }
+    };
+
+    return (
+      <div className="App">
+        <Navbar></Navbar>
+        <h1>React - state</h1>
+        <p>Count is: {this.clickCount}</p>
+        <button onClick={clickHandler}> Click me </button>
+        <User
+          theColor={this.state.backColor}
+          firstName={this.state.userA.firstName}
+          image={this.state.userA.avatarUrl}
+          bootcampName={this.state.bootcamp} />
+        <User
+          firstName={this.state.userB.firstName}
+          image={this.state.userB.avatarUrl}
+          bootcampName={this.state.bootcamp} />
+      </div>
+    );
   }
-
-  const [clickCount, setState] = useState(0);
-
-  let colorMapper = () => {
-    const hexColor = "#" + Math.floor(Math.random() * 16777215).toString(16)
-    return hexColor
-  }
-
-  let clickHandler = () => {
-    if (clickCount % 5 === 0) {
-      setState({
-        backColor: colorMapper(),
-        clickCount: clickCount + 1
-      })
-    }
-    else {
-      setState({ clickCount: clickCount + 1 })
-    }
-  }
-
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <h1>React - state</h1>
-      <p>Count is: {clickCount}</p>
-      <button onClick={clickHandler}> Click me </button>
-      <User
-        theColor={state.backColor}
-        firstName={state.userA.firstName}
-        image={state.userA.avatarUrl}
-        bootcampName={state.bootcamp}
-      />
-      <User
-        firstName={state.userB.firstName}
-        image={state.userB.avatarUrl}
-        bootcampName={state.bootcamp}
-      />
-    </div>
-  )
 }
 
 export default App
--- src/components/User.js
+++ src/components/User.js
@@ -6,7 +6,7 @@ function User(props) {
             <h2 style={{ backgroundColor: props.theColor }}>
                 Hello, {props.firstName}
             </h2>
-            <img src={props.image} width="350" height="350" />
+            <img src={props.image} width="350" height="350" alt="#" />
             <h2>Welcome to {props.bootcampName}</h2>
         </div>
     )
